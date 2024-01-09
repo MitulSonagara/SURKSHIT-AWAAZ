988dc5eb096e0351dec62e6d75ffd2ebd7cd6003
Mitul Sonagara <mitulkumarsongara@gmail.com>
2024-01-10 00:16:53 +0530

    v0

--- user/src/routes/feedback.js
+++ user/src/routes/feedback.js
@@ -1,8 +1,17 @@
 const express = require("express")
 const router = express.Router()
+const FeedbackQuestion = require('../models/feedbackQuestions');
+const feedbackResponses = require("../models/feedbackResponses")
 
-router.get("/", (req, res) => {
-    res.render("feedback");
+router.get("/", async (req, res) => {
+    try {
+        // Retrieve questions from the database
+        const questions = await FeedbackQuestion.find()
+        res.render('feedback', { questions });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 })
 
 module.exports = router
